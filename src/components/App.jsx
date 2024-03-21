0e176ee93a5b182d9772482699602fc1d1301c31
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2024-03-21 17:00:43 +0200

    about mobile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Hero from './hero-section/Hero';
 import scss from './App.module.scss';
+import About from './about-section/About';
 
 export const App = () => {
   return (
     <div className={scss.container}>
       <Hero />
+      <About />
     </div>
   );
 };
